cce45227d12ba4ba5eb561b78c02289b704bf663
Hulkdemon <al01-077-0321@utdelacosta.edu.mx>
2025-08-15 14:14:11 -0700

    basic structure config models, migrations and controllers

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Task extends Model
 {
-    //
+    protected $fillable = ['name', 'project_id', 'priority'];
+
+    protected $casts = [
+        'priority' => 'integer',
+        'project_id' => 'integer',
+    ];
+
+    public function project(): BelongsTo
+    {
+        return $this->belongsTo(Project::class);
+    }
 }
